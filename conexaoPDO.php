f38acf83a05e6b8a9e4ad6e38e13c65d646b9a3c
Crisley Duarte <cryslley17@gmail.com>
2021-07-20 08:13:15 -0300

    Add files via upload

---
+++ conexaoPDO.php
@@ -0,0 +1,15 @@
+<?php
+
+try
+{
+$pdo = new PDO('mysql:host=;dbname=', 'root', '', array(PDO::MYSQL_ATTR_LOCAL_INFILE => true,));
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->exec('SET NAMES "utf8"');
+}
+catch (PDOException $e)
+{
+$error = 'Unable to connect to the database server' . $e ;
+include 'error.html.php';
+}
+
+?>
